07027c1fcdadd4fcc052a76c423b7d0f2bad3fd2
it1906 <githublmao@seznam.cz>
2022-10-02 14:46:11 +0200

    07: Vypis dat ze Strapi - 2

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,24 +1,33 @@
 import React from "react";
-import useFetch from "../hooks/useFetch";
-import {Link} from 'react-router-dom';
+import { useEffect, useState } from "react"
 
 export default function Homepage() {
-  const {loading, error, data} = useFetch('http://localhost:1337/api/recipes')
 
-  if(loading) return <p>Načítání...</p>
-  if(error) return <p>Chyba</p>
+  const [recipe, setRecipe] = useState([]);
 
-  console.log(data)
+  useEffect(()=>{
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+  const api = await fetch (`http://localhost:1337/api/recipes`);
+  const data = await api.json();
+  setRecipe(data.data);
+  };
 
   return (
-    <div>
-      {data.map(recipe => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <p>{recipe.description.substring(0, 200)}...</p>
-          <Link to={`/recipes/${recipe.id}`}>Vice...</Link>
-        </div>
-      ))}
+    <div key={recipe.id}>
+      {recipe.map((recipe)=>{
+        return(
+          <ul>
+            <li>
+              <h5>{recipe.attributes.name}</h5>
+              <p>{recipe.attributes.description}</p>
+            </li>
+
+          </ul>
+        );
+      })}
     </div>
   )
 }
